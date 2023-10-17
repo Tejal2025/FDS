968c3c197bae5c9ad6abe5add1b0434b294664f5
Tejal2025 <147473934+Tejal2025@users.noreply.github.com>
2023-10-17 14:26:51 +0530

    Update SetB1.py

--- FDS/Assignment no 3/SetB1.py
+++ FDS/Assignment no 3/SetB1.py
@@ -3,10 +3,13 @@ import numpy as np
 from sklearn import preprocessing
 import scipy.stats as s
 
+#1
 df=pd.read_csv("file:///data/ty8/Ty%208/FDS/Assignment%20no%203/winequality-red.csv")
 print(df)
 
-print("Rescalling Data")
+#2
+#Rescaling
+print("Rescaling Data")
 print("\n\n Data scaled between 0 and 1")
 data_scaler=preprocessing.MinMaxScaler(feature_range=(0,1))
 data_scaled =data_scaler.fit_transform(df)
@@ -14,8 +17,9 @@ print("\n Min Max Scaled data")
 print("----------------------------------")
 print(data_scaled.round(2))
 
-#Standarding data
-print("\n Standarding Data")
+#3
+#Standardizing data
+print("\n Standardizing Data")
 print("--------------------")
 arr=np.array(df)
 print(arr)
@@ -24,16 +28,20 @@ print("\n initial standard devetion=",round(arr.std(),2))
 X_scaled=preprocessing.scale(arr)
 X_scaled.mean(axis=0)
 X_scaled.std(axis=0)
-print("\n standardizing data=\n",X_scaled.round(2))
+print("\n standarding data=\n",X_scaled.round(2))
 print("\n scaling mean :",s.tmean(X_scaled).round(2))
 print("\n scaling standard deviation :",round(X_scaled.std(),2))
 
+#4
+#Normalizing Data
 print("Normalization rescales such that sum of each row is 1:")
 dn=preprocessing.normalize(df,norm='l1')
 print("-------------------------------")
 print(dn.round(2))
 
-print("binarize data(Make Binary)")
+#5
+#Binarizing Data
+print("binarizer data(Make Binary)")
 data_binarized=preprocessing.Binarizer(threshold=5).transform(df)
 print("\n Binarized Data")
 print("-----------------")
